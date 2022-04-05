4de508c2a62d2d7b6f43efc6c946f1f5e8eb2727
Samuel Guedes <91099488+1Samel2@users.noreply.github.com>
2022-04-05 16:04:31 -0300

    Create past

---
+++ src/page/Home/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import { Container, Navigation, Redirect} from './styles';
+
+function Home() {
+
+  return (
+    <Container>
+      <Navigation>
+        <ul>
+          <Redirect> 
+
+          </Redirect>
+        </ul>
+      </Navigation>
+
+
+    </Container>
+
+  );
+}
+
+export default Home;
---
+++ src/page/Home/styles.js
@@ -0,0 +1,24 @@
+import styled from 'styled-component'
+
+
+
+
+export const Container = styled.div`
+
+    background: black;
+
+
+`;
+
+export const Navigation = styled.section`
+
+
+
+`;
+
+export const Redirect = styled.li`
+
+
+`;
+
+
